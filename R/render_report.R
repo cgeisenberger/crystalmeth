9ad33ad98f70c0eaf99e2c9065d08bfcffb63f78
Christoph Geisenberger <cgeisenberger@tests-MacBook-Pro.local>
2020-08-25 20:11:23 +0200

    Updated render_report(), moved within package structure

---
+++ R/render_report.R
@@ -0,0 +1,21 @@
+#' Render diagnostic report for ClassificationCase object
+#'
+#' Wrapper around rmarkdown::render(). Clones the ClassificationCase object into
+#' a new environment which is passed to rmarkdown::render().
+#'
+#' @param case Object of type ClassificationCase.
+#' @param input Report template (.Rmd file) (passed to rmarkdown::render).
+#' @param output_format Output directory (passed to rmarkdown::render).
+#' @param output_file Name of output file (passed to rmarkdown::render).
+#' @param output_dir Output directory of rendered file (passed to rmarkdown::render).
+#' @export
+
+render_report <- function(case, ...){
+
+  # store case object new environment (necessary to pass data to render())
+  report_env <- new.env()
+  assign(x = "case", value = case$clone(), envir = report_env)
+
+  # render report
+  rmarkdown::render(envir = report_env, ...)
+}
